66f32d387bd65fbc880589ed066084ec0af5fef5
Rafael Mata <rafaelmata357@gmail.com>
2021-03-19 12:28:58 -0600

    Feat: Complete tokenize and model creationg process

--- models/train_classifier.py
+++ models/train_classifier.py
@@ -3,16 +3,15 @@
 #
 # PROGRAMMER:    Rafael Mata M.
 # DATE CREATED:  12 Marh 2021                                
-# REVISED DATE:  12 March 2021
+# REVISED DATE:  19 March 2021
 # PURPOSE: A ML Pipeline script to predict Categories form disastern messages and store in a file
 #
-#  Usage: python3 process_data.py messages_filepath  categories_filepath database_filename
+#  Usage: python3 train_classifier.py database_filename
 #
 #
 
-# Imports python modules
+# import libraries
 
-import sys
 import json
 import pandas as pd
 import re
@@ -24,6 +23,19 @@ from nltk.tokenize import word_tokenize
 from sqlalchemy import create_engine
 import nltk
 from nltk.corpus import stopwords
+nltk.download('wordnet')
+
+from sklearn.metrics import confusion_matrix
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.pipeline import Pipeline
+import numpy as np
+from sklearn.metrics import f1_score
+from sklearn.metrics import classification_report, accuracy_score
+from sklearn.multioutput import MultiOutputClassifier
+from sklearn.model_selection import GridSearchCV
+import joblib
 
 
 def load_data(database_filepath):
@@ -36,30 +48,121 @@ def load_data(database_filepath):
         Returns:
         --------
         df : pandas data frame
-        
     '''
 
-    engine = create_engine('sqlite:///'.foramt(database_filepath))
+    
+    engine = create_engine('sqlite:///{}'.format(database_path))
     df = pd.read_sql('SELECT * FROM MESSAGES', engine)
+    X = df.message
+    y = df.iloc[:,4:]
+    category_names  = list(y.columns)
+    
+    return X, y, category_names
 
-    returns df
 
 
 def tokenize(text):
-    pass
+    '''Fucntion to tokenize and clean the text
+    
+       Params:
+       -------
+       text : str, the text message
+
+       Returns:
+       -------
+       clean_token: list, with the clean tokens
+    '''
+
+        
+    # Normalize text
+    text = re.sub(r"[^a-zA-Z0-9]", " ", text.lower())
+
+    # tokenize text
+    tokens = word_tokenize(text)
+    
+    # Remove stop words
+    tokens = [w for w in tokens if w not in stopwords.words('english')]
+    
+    # initiate lemmatizer
+    lemmatizer = WordNetLemmatizer()
+
+    # iterate through each token
+    clean_tokens = []
+    for tok in tokens:
+        
+        # lemmatize, normalize case, and remove leading/trailing white space
+        clean_tok = lemmatizer.lemmatize(tok).strip()
+       
+    
+        clean_tokens.append(clean_tok)
+  
+    return clean_tokens
 
 
 def build_model():
-    pass
+    '''Function to build the Model Pipeline
+    
+       Params:
+       -------
+       None
+
+       Returns:
+       ---------
+       pipeline: sklearn pipeline model
+    '''
+
+    pipeline = Pipeline([
+        ('vect', CountVectorizer(tokenizer=tokenize)),
+        ('tfidf', TfidfTransformer()),
+        ('clf', MultiOutputClassifier(RandomForestClassifier()))
+    ])
+
+    return pipeline
 
 
 def evaluate_model(model, X_test, Y_test, category_names):
-    pass
+    ''' Function to evaluate the trainned model 
+    
+        Params:
+        -------
+        model: pipeline sklearn model
+        X_test: numpy array, X test values
+        Y_test: numpy array, Y labels
+        category_names: list, category names
+
+        Returns:
+        ---------
+        None
+    
+    '''
+
+    print('accuracy {}'.format((y_pred == y_test.values).mean()))
+
+    for i in range(len(y_test.values[0,:])):
+    print('Target: {}'.format(category_names[i]))
+    print('--'*30)
+    print(classification_report(y_test.values[:,i], y_pred[:,i]))
+
+    return None
 
 
 def save_model(model, model_filepath):
-    pass
 
+    ''' Function to save the model using joblib in pickle format 
+    
+        Params:
+        ---------
+        model: sklearn pipeline, trainned model
+        model_filepath : str, file path
+
+        Returns:
+        ---------
+        None
+    '''
+
+    joblib.dump(pipeline, model_filepath, compress = 1)
+
+    return None
 
 def main():
     if len(sys.argv) == 3:
